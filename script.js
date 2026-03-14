6d53c4f1ca51fa2a493cd3870a34bcfa5509ae93
Navaneeth6374 <navaneeth4002@gmail.com>
2026-03-14 12:11:29 +0530

    Updated portfolio with certificates and scripts

---
+++ script.js
@@ -0,0 +1,42 @@
+const text = [
+"Beginner Software Developer",
+"Java Developer",
+"React.js Learner",
+"Web Developer"
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type(){
+
+if(count === text.length){
+count = 0;
+}
+
+currentText = text[count];
+letter = currentText.slice(0, ++index);
+
+document.querySelector(".typing").textContent = letter;
+
+if(letter.length === currentText.length){
+count++;
+index = 0;
+setTimeout(type, 1000);
+}
+else{
+setTimeout(type, 80);
+}
+
+})();
+
+function openPopup(src){
+document.getElementById("popup").style.display="flex";
+document.getElementById("popup-img").src=src;
+}
+
+function closePopup(){
+document.getElementById("popup").style.display="none";
+}
